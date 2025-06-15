d12f39afecff0187118ed4b06f6d16e943ba00a4
Martina <martina.cesarini97@gmail.com>
2025-06-15 20:39:57 +0200

    Paint Scene
    Paint Scene - updated
    Made changes to the Home scene
    Added UI tutorials in every scenes

---
+++ Assets/Script/Paint/LevitateOnIdle.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable))]
+public class LevitateOnIdle : MonoBehaviour
+{
+    public float amplitude = 0.25f;         // How far it moves up and down
+    public float frequency = 1f;            // How fast it moves
+    public float massMultiplierOnGrab = 3f; // Mass multiplier when grabbed
+
+    private Rigidbody rb;
+    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable;
+    private float originalMass;
+    private Vector3 startPosition;
+    private float timeOffset;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+
+        originalMass = rb.mass;
+        startPosition = transform.position;
+        timeOffset = Random.Range(0f, 2f * Mathf.PI); // Stagger bobbing
+
+        grabInteractable.selectEntered.AddListener(OnGrab);
+        grabInteractable.selectExited.AddListener(OnRelease);
+
+        // Start levitating
+        rb.isKinematic = true;
+        rb.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (rb.isKinematic)
+        {
+            float newY = startPosition.y + Mathf.Sin((Time.time + timeOffset) * frequency) * amplitude;
+            Vector3 newPosition = new Vector3(startPosition.x, newY, startPosition.z);
+            rb.MovePosition(newPosition);
+        }
+    }
+
+    private void OnGrab(SelectEnterEventArgs args)
+    {
+        rb.isKinematic = false;
+        rb.useGravity = true;
+        rb.mass = originalMass * massMultiplierOnGrab;
+    }
+
+    private void OnRelease(SelectExitEventArgs args)
+    {
+        startPosition = transform.position; // Reset float origin
+        timeOffset = Random.Range(0f, 2f * Mathf.PI);
+        rb.isKinematic = true;
+        rb.useGravity = false;
+        rb.mass = originalMass;
+    }
+
+    void OnDestroy()
+    {
+        grabInteractable.selectEntered.RemoveListener(OnGrab);
+        grabInteractable.selectExited.RemoveListener(OnRelease);
+    }
+}
