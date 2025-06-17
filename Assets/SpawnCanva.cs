1763105262bf210ab808e4bfd62fb721fe7d9efc
72Gregorio72 <gregoriopicchio@gmail.com>
2025-06-17 16:37:51 +0200

    2

---
+++ Assets/SpawnCanva.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class SpawnCanva : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ActivateObject()
+    {
+        if (this.gameObject.activeSelf)
+        {
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            this.gameObject.SetActive(true);
+        }
+    }
+}
